c12adaa3e07154728c7250b0fd60c4b06796ea27
Nickolay Martynenko <nickolaymartynenko@MacBook-Air-Nickolay.local>
2024-11-17 15:39:50 +0300

    Update preprocessing utilities

--- dataset/utilities/make_dataset.py
+++ dataset/utilities/make_dataset.py
@@ -28,22 +28,27 @@ if args.preprocesser:
 	kwargs.update({"preprocesser": preprocesser})
 	if args.preprocesser == "rebin":
 		kwargs.update({"preprocesser_kwargs": {"regime": args.regime}})
+print("") # print blank line to make output look nice
 
 SwiftXRTdict = read_SwiftXRT(args.source_directory)
+print("") # print blank line to make output look nice
+
 train, val, test = make_dataset(SwiftXRTdict, **kwargs)
+print("") # print blank line to make output look nice
+
 for dataframe in (train, val, test):
 	if not os.path.isdir(f"{dataframe.name}"):
 		os.mkdir(f"{dataframe.name}")
 	dataframe.to_csv(f"{dataframe.name}/{args.name}.csv")
 print(
 	f"Dataset created:\n"+
-	f"└── {os.getcwd()}\n"+
-	f"    └── train\n"+
-	f"        └── {args.name}.csv\n"+
-	f"    └── val\n"+
-	f"        └── {args.name}.csv\n"+
-	f"    └── test\n"+
-	f"        └── {args.name}.csv\n"
+	f".\n"+
+	f"└── train\n"+
+	f"    └── {args.name}.csv\t({len(train)} entries)\n"+
+	f"└── val\n"+
+	f"    └── {args.name}.csv\t({len(val)} entries)\n"+
+	f"└── test\n"+
+	f"    └── {args.name}.csv\t({len(test)} entries)\n"
 )
 
 
