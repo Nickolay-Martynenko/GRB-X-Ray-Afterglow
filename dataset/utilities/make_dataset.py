cb06426d05de93190501fdc14382258dc31aa875
Nickolay Martynenko <nickolaymartynenko@MacBook-Air-Nickolay.local>
2024-11-17 15:25:02 +0300

    Update preprocessing utilities

--- dataset/utilities/make_dataset.py
+++ dataset/utilities/make_dataset.py
@@ -7,9 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--source_directory", type=str,
 					help="the directory to read data from"
 )
-parser.add_argument("-t", "--target_directory", type=str,
-					default=".",
-					help="the directory to create dataset in"
+parser.add_argument("-n", "--name", type=str,
+					help="the name of output .csv files"
 )
 parser.add_argument("-p", "--preprocesser", type=str,
 					choices=["rebin", "extract_features"],
@@ -33,10 +32,19 @@ if args.preprocesser:
 SwiftXRTdict = read_SwiftXRT(args.source_directory)
 train, val, test = make_dataset(SwiftXRTdict, **kwargs)
 for dataframe in (train, val, test):
-	if not os.path.isdir(f"{args.target_directory}"):
-		os.mkdir(f"{args.target_directory}")
-	dataframe.to_csv(f"{args.target_directory}/{dataframe.name}.csv")
-print(f"Dataset created in directory '{args.target_directory}/...'")
+	if not os.path.isdir(f"{dataframe.name}"):
+		os.mkdir(f"{dataframe.name}")
+	dataframe.to_csv(f"{dataframe.name}/{args.name}.csv")
+print(
+	f"Dataset created:\n"+
+	f"└── {os.getcwd()}\n"+
+	f"    └── train\n"+
+	f"        └── {args.name}.csv\n"+
+	f"    └── val\n"+
+	f"        └── {args.name}.csv\n"+
+	f"    └── test\n"+
+	f"        └── {args.name}.csv\n"
+)
 
 
 
